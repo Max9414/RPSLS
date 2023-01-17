0370086693cf1882fecf0540341ab1fc027232af
Max94 <maxy_14@hotmail.it>
2023-01-17 11:37:47 +0000

    added function for the three games, but no code yet

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+/**
+ * This function will create the whole HTML necessary for the first game to work
+ * The game in question is the normal one, user can select the card normally and play the game
+ */
