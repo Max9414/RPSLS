297f6bab21b91ffb790f019f93894589fa9595a5
Max94 <maxy_14@hotmail.it>
2023-02-07 16:11:58 +0000

    Adjusted indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,27 @@
-
 let cards = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
 window.onload = (event) => {
-    console.log("page is fully loaded");
-    // Event listeners for the 3 different games and the games explanation
-    document.getElementById('game-btn1').addEventListener('click', playOriginalGame);
-    document.getElementById('game-btn2').addEventListener('click', playFivevFiveGame);
-    document.getElementById('game-btn3').addEventListener('click', playRandomGame);
-    document.getElementById('games-info').addEventListener('click', gamesExplained);
-    //Added listener to reset the score
-    document.getElementsByTagName('header')[0].addEventListener('click', resetPoints);
-  };
+  console.log("page is fully loaded");
+  // Event listeners for the 3 different games and the games explanation
+  document.getElementById('game-btn1').addEventListener('click', playOriginalGame);
+  document.getElementById('game-btn2').addEventListener('click', playFivevFiveGame);
+  document.getElementById('game-btn3').addEventListener('click', playRandomGame);
+  document.getElementById('games-info').addEventListener('click', gamesExplained);
+  //Added listener to reset the score
+  document.getElementsByTagName('header')[0].addEventListener('click', resetPoints);
+};
 
 
 
-  
-  let selectedCards= [];
-  let gameType = '';
+
+let selectedCards = [];
+let gameType = '';
 
 /**
  * This function will be used to generate a random pick for the pc for all games 
  */
-function randomCard () {
-    return cards[Math.floor(Math.random() * cards.length)];
+function randomCard() {
+  return cards[Math.floor(Math.random() * cards.length)];
 }
 
 /** 
@@ -30,16 +29,16 @@ function randomCard () {
  * It then generates an array of cards based on the random numbers generated.
  */
 
-function fiveRandomGenerated () {
+function fiveRandomGenerated() {
   let numbers = [0, 1, 2, 3, 4];
   let randomNumber = [];
   let randomCards = [];
-  for (let i = 0; i<5; i++) {
-    let randomIndex = Math.floor(Math.random()* numbers.length);
+  for (let i = 0; i < 5; i++) {
+    let randomIndex = Math.floor(Math.random() * numbers.length);
     randomNumber.push(numbers[randomIndex]);
     numbers.splice(randomIndex, 1);
   }
-  for (let j = 0; j<5; j++) {
+  for (let j = 0; j < 5; j++) {
     randomCards.push(cards[randomNumber[j]]);
   }
   return randomCards;
@@ -53,18 +52,18 @@ function cardsCreation(game) {
   let numbers = [0, 1, 2, 3, 4];
   let randomNumber = [];
   let randomCards = [];
-  for (let i = 0; i<3; i++) {
-    let randomIndex = Math.floor(Math.random()* numbers.length);
+  for (let i = 0; i < 3; i++) {
+    let randomIndex = Math.floor(Math.random() * numbers.length);
     randomNumber.push(numbers[randomIndex]);
     numbers.splice(randomIndex, 1);
   }
-  for (let j = 0; j<3; j++) {
+  for (let j = 0; j < 3; j++) {
     randomCards.push(cards[randomNumber[j]]);
-    game.innerHTML+=`
+    game.innerHTML += `
       <button class="card">
       <img src="assets/image/${cards[randomNumber[j]]}.png" alt="${cards[randomNumber[j]]}"  id="${cards[randomNumber[j]]}">
       </button>`;
-}
+  }
   return game;
 }
 
@@ -73,16 +72,16 @@ function cardsCreation(game) {
  * It works, with a for function, for the 5v5 game as well.
  */
 
-function solution (player, pc) {
-    if (player == pc ) return "it's a tie!";
-    let winCondition = {
-      rock: ['lizard', 'scissors'],
-      lizard: ['spock', 'paper'],
-      spock: ['scissors', 'rock'],
-      scissors: ['paper', 'lizard'],
-      paper: ['rock', 'spock'],
-    };
-    return winCondition[player].includes(pc) ? "The player wins!" : "The pc wins!";
+function solution(player, pc) {
+  if (player == pc) return "it's a tie!";
+  let winCondition = {
+    rock: ['lizard', 'scissors'],
+    lizard: ['spock', 'paper'],
+    spock: ['scissors', 'rock'],
+    scissors: ['paper', 'lizard'],
+    paper: ['rock', 'spock'],
+  };
+  return winCondition[player].includes(pc) ? "The player wins!" : "The pc wins!";
 }
 
 /**
@@ -93,45 +92,45 @@ function solution (player, pc) {
  * selection.
  */
 function fullSelection(event) {
-    let playerCard = event.target.id;
-    console.log(playerCard);
-    let index = selectedCards.indexOf(playerCard);
-    if (index === -1) {
-      selectedCards.push(playerCard);
-      document.getElementById(playerCard).src = "assets/image/b" + playerCard + ".png";
-    } else { 
-      selectedCards.splice(index, 1);
-      document.getElementById(playerCard).src = "assets/image/" + playerCard + ".png";
-    }
-    console.log(selectedCards);
-    console.log(selectedCards.length);
-    if (selectedCards.length == 5) {
-      // let confirmed = confirm ("Are you sure you want to select these cards?");
-      // if (confirmed)
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, select it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
+  let playerCard = event.target.id;
+  console.log(playerCard);
+  let index = selectedCards.indexOf(playerCard);
+  if (index === -1) {
+    selectedCards.push(playerCard);
+    document.getElementById(playerCard).src = "assets/image/b" + playerCard + ".png";
+  } else {
+    selectedCards.splice(index, 1);
+    document.getElementById(playerCard).src = "assets/image/" + playerCard + ".png";
+  }
+  console.log(selectedCards);
+  console.log(selectedCards.length);
+  if (selectedCards.length == 5) {
+    // let confirmed = confirm ("Are you sure you want to select these cards?");
+    // if (confirmed)
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, select it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
         let finalSolution = [];
         let fiveRandom = fiveRandomGenerated();
         console.log(fiveRandom);
-        for (let i=0; i<fiveRandom.length; i++ ) {
+        for (let i = 0; i < fiveRandom.length; i++) {
           let sol = solution(selectedCards[i], fiveRandom[i]);
           if (sol === "The player wins!") {
-          console.log(sol);
-          document.getElementById('player-score').innerHTML = parseInt(document.getElementById('player-score').innerHTML) + 1;
-          finalSolution.push('v');
-        } else if (sol === "The pc wins!" ) {
-          console.log(sol);
-          document.getElementById('computer-score').innerHTML = parseInt(document.getElementById('computer-score').innerHTML) + 1;
-          finalSolution.push('n');
-        } else {}
-       }    
+            console.log(sol);
+            document.getElementById('player-score').innerHTML = parseInt(document.getElementById('player-score').innerHTML) + 1;
+            finalSolution.push('v');
+          } else if (sol === "The pc wins!") {
+            console.log(sol);
+            document.getElementById('computer-score').innerHTML = parseInt(document.getElementById('computer-score').innerHTML) + 1;
+            finalSolution.push('n');
+          } else {}
+        }
         winLoseTieFive(selectedCards, fiveRandom, finalSolution);
         selectedCards = [];
         document.getElementById('rock').src = "assets/image/rock.png";
@@ -139,61 +138,63 @@ function fullSelection(event) {
         document.getElementById('scissors').src = "assets/image/scissors.png";
         document.getElementById('lizard').src = "assets/image/lizard.png";
         document.getElementById('spock').src = "assets/image/spock.png";
-      }
-       else {
+      } else {
         Swal.fire({
           timer: 1300,
-          title:'Cancelled',
-          text:'Your selection has been resetted! :)'})
-        selectedCards = [] ;
+          title: 'Cancelled',
+          text: 'Your selection has been resetted! :)'
+        })
+        selectedCards = [];
         document.getElementById('rock').src = "assets/image/rock.png";
         document.getElementById('paper').src = "assets/image/paper.png";
         document.getElementById('scissors').src = "assets/image/scissors.png";
         document.getElementById('lizard').src = "assets/image/lizard.png";
         document.getElementById('spock').src = "assets/image/spock.png";
-       }
-      })
+      }
+    })
 
-  }
-     else {}
+  } else {}
 }
 
 
 
 // card selection check
 function cardSelected(event) {
-    let playerCard = event.target.id;
-    console.log(playerCard);
-    let pcCard = randomCard();
-    let sol = solution(playerCard, pcCard);
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      imageUrl: `assets/image/${playerCard}.png`,
-      imageWidth: 250,
-      imageHeight: 200,
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, select it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        console.log(pcCard);
-        if (sol === "The player wins!") {
-          console.log(sol);
-          document.getElementById('player-score').innerHTML = parseInt(document.getElementById('player-score').innerHTML) + 1;
-          winnerLoserTiePage(playerCard, pcCard, sol);
-        } else if (sol === "The pc wins!" ) {
-          console.log(sol);
-          document.getElementById('computer-score').innerHTML = parseInt(document.getElementById('computer-score').innerHTML) + 1;
-          winnerLoserTiePage(playerCard, pcCard, sol);
-        } else {winnerLoserTiePage(playerCard, pcCard, sol);}
+  let playerCard = event.target.id;
+  console.log(playerCard);
+  let pcCard = randomCard();
+  let sol = solution(playerCard, pcCard);
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    imageUrl: `assets/image/${playerCard}.png`,
+    imageWidth: 250,
+    imageHeight: 200,
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, select it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log(pcCard);
+      if (sol === "The player wins!") {
+        console.log(sol);
+        document.getElementById('player-score').innerHTML = parseInt(document.getElementById('player-score').innerHTML) + 1;
+        winnerLoserTiePage(playerCard, pcCard, sol);
+      } else if (sol === "The pc wins!") {
+        console.log(sol);
+        document.getElementById('computer-score').innerHTML = parseInt(document.getElementById('computer-score').innerHTML) + 1;
+        winnerLoserTiePage(playerCard, pcCard, sol);
+      } else {
+        winnerLoserTiePage(playerCard, pcCard, sol);
+      }
     } else {
-        console.log("select again");
-        Swal.fire({
-          timer: 1300,
-          title:'Cancelled!',
-          text:'Your selection has been resetted! :)'})
+      console.log("select again");
+      Swal.fire({
+        timer: 1300,
+        title: 'Cancelled!',
+        text: 'Your selection has been resetted! :)'
+      })
     }
   })
 }
@@ -204,32 +205,32 @@ function cardSelected(event) {
  */
 function winnerLoserTiePage(playerCard, pcCard, sol) {
   let game = document.getElementById('game-area');
-    game.classList.add('showcase-section');
-    if (sol === 'The player wins!') {
-      game.innerHTML = `
+  game.classList.add('showcase-section');
+  if (sol === 'The player wins!') {
+    game.innerHTML = `
       <h1>You win!!</h1>`;
-    } else if (sol === 'The pc wins!') {
-        game.innerHTML = `
+  } else if (sol === 'The pc wins!') {
+    game.innerHTML = `
         <h1>You lose!!</h1>`;
-    } else {
-      game.innerHTML = `
+  } else {
+    game.innerHTML = `
       <h1>It's a tie!!</h1>`;
-    }
-    game.innerHTML += `<div id="win-lose-div" class="showcase-cards"></div>`;
-    let winner = document.getElementById('win-lose-div');
-    winner.innerHTML += `
+  }
+  game.innerHTML += `<div id="win-lose-div" class="showcase-cards"></div>`;
+  let winner = document.getElementById('win-lose-div');
+  winner.innerHTML += `
       <button class="card">
         <img src="assets/image/${playerCard}.png" alt="${playerCard}"  id="${playerCard}">
       </button>
       <button class="card">
         <img src="assets/image/${pcCard}.png" alt="${pcCard}"  id="${pcCard}">
       </button>`;
-if (gameType === 'original') {
-  setTimeout(playOriginalGame, 1500);
-} else {
-  setTimeout(playRandomGame, 1500);
-}
-  
+  if (gameType === 'original') {
+    setTimeout(playOriginalGame, 1500);
+  } else {
+    setTimeout(playRandomGame, 1500);
+  }
+
 }
 
 /**
@@ -241,7 +242,7 @@ function winLoseTieFive(player, pc, solution) {
   let game = document.getElementById('game-area');
   game.innerHTML = ``;
   game.classList.add('showcase-section');
-  for (let i=0; i<5; i++) {
+  for (let i = 0; i < 5; i++) {
     let beginning = `
       <div class="showcase-cards smaller">
         <button class="card">
@@ -271,11 +272,11 @@ function winLoseTieFive(player, pc, solution) {
  * The game in question is the normal one, user can select the card normally and play the game
  */
 function playOriginalGame(event) {
-    let game = document.getElementById('game-area');
-    gameType = 'original';
-    game.classList.remove('showcase-section');
-    game.classList.add('cards-bttm');
-    game.innerHTML = `
+  let game = document.getElementById('game-area');
+  gameType = 'original';
+  game.classList.remove('showcase-section');
+  game.classList.add('cards-bttm');
+  game.innerHTML = `
     <div>
       <button id="home-btn">Home</button>
     </div>
@@ -296,16 +297,16 @@ function playOriginalGame(event) {
         <img src="assets/image/spock.png" alt="spock" id="spock">
       </button>
     </div>`;
-    document.getElementById('home-btn').addEventListener('click', homeGenerator);
+  document.getElementById('home-btn').addEventListener('click', homeGenerator);
 
-    let cards = document.getElementsByClassName('card');
-    console.log(cards);
+  let cards = document.getElementsByClassName('card');
+  console.log(cards);
 
-    // Event listener for chosen card by player
-    for (let j=0; j<cards.length; j++) {
-      cards[j].addEventListener('click', cardSelected);
-      console.log(cards[j]);
-}
+  // Event listener for chosen card by player
+  for (let j = 0; j < cards.length; j++) {
+    cards[j].addEventListener('click', cardSelected);
+    console.log(cards[j]);
+  }
 }
 
 /**
@@ -314,12 +315,12 @@ function playOriginalGame(event) {
  * and will be played one at a time, in order
  */
 function playFivevFiveGame(event) {
-    let game = document.getElementById('game-area');
-    selectedCards = [];
-    gameType = 'five';
-    game.classList.remove('showcase-section');
-    game.classList.add('cards-bttm');
-    game.innerHTML = `
+  let game = document.getElementById('game-area');
+  selectedCards = [];
+  gameType = 'five';
+  game.classList.remove('showcase-section');
+  game.classList.add('cards-bttm');
+  game.innerHTML = `
     <div>
       <button id="home-btn">Home</button>
       <button id="reset-btn">Reset</button>
@@ -341,18 +342,18 @@ function playFivevFiveGame(event) {
         <img src="assets/image/spock.png" alt="spock" id="spock">
       </button>
     </div>`;
-    document.getElementById('home-btn').addEventListener('click', homeGenerator);
-    document.getElementById('reset-btn').addEventListener('click', playFivevFiveGame);
-   
-    
+  document.getElementById('home-btn').addEventListener('click', homeGenerator);
+  document.getElementById('reset-btn').addEventListener('click', playFivevFiveGame);
 
-    let cards = document.getElementsByClassName('card');
-    console.log(cards);
 
-    // Event listener with function to wait for all 5 cards to be selected
-    for (let j=0; j<cards.length; j++) {
+
+  let cards = document.getElementsByClassName('card');
+  console.log(cards);
+
+  // Event listener with function to wait for all 5 cards to be selected
+  for (let j = 0; j < cards.length; j++) {
     cards[j].addEventListener('click', fullSelection);
-}
+  }
 }
 
 /**
@@ -361,25 +362,25 @@ function playFivevFiveGame(event) {
  * for the pc, then they will have to play like the normal versione, one card at a time
  */
 function playRandomGame(event) {
-     let game = document.getElementById('game-area');
-     gameType = 'random';
-     game.innerHTML = `
+  let game = document.getElementById('game-area');
+  gameType = 'random';
+  game.innerHTML = `
      <div>
       <button id="home-btn">Home</button>
       <button id="shuffle-btn">Shuffle</button>
      </div>
      <div id="game-area-div">
      </div>`;
-     game.classList.remove('showcase-section');
-     game.classList.add('cards-bttm');
-     let gameDiv = document.getElementById('game-area-div');
-     gameDiv += cardsCreation(gameDiv);
-     let cards = document.getElementsByClassName('card');
-     for (let i=0; i<cards.length; i++) {
-      cards[i].addEventListener('click', cardSelected);
-     }
-     document.getElementById('home-btn').addEventListener('click', homeGenerator);
-     document.getElementById('shuffle-btn').addEventListener('click', playRandomGame);
+  game.classList.remove('showcase-section');
+  game.classList.add('cards-bttm');
+  let gameDiv = document.getElementById('game-area-div');
+  gameDiv += cardsCreation(gameDiv);
+  let cards = document.getElementsByClassName('card');
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', cardSelected);
+  }
+  document.getElementById('home-btn').addEventListener('click', homeGenerator);
+  document.getElementById('shuffle-btn').addEventListener('click', playRandomGame);
 }
 
 /**
@@ -388,10 +389,10 @@ function playRandomGame(event) {
  */
 
 function homeGenerator(event) {
-    let game = document.getElementById('game-area');
-    gameType='';
-    game.classList.remove('explanation');
-    game.innerHTML = `
+  let game = document.getElementById('game-area');
+  gameType = '';
+  game.classList.remove('explanation');
+  game.innerHTML = `
     <div class="buttons">
       <button id="game-btn1">Original</button>
       <button id="game-btn2">5 vs 5</button>
@@ -403,16 +404,16 @@ function homeGenerator(event) {
     <div>
       <h2>Choose a game above: <span id="games-info">games info</span></h2>
     </div>`;
-    document.getElementById('game-btn1').addEventListener('click', playOriginalGame);
-    document.getElementById('game-btn2').addEventListener('click', playFivevFiveGame);
-    document.getElementById('game-btn3').addEventListener('click', playRandomGame);
-    document.getElementById('games-info').addEventListener('click', gamesExplained);
+  document.getElementById('game-btn1').addEventListener('click', playOriginalGame);
+  document.getElementById('game-btn2').addEventListener('click', playFivevFiveGame);
+  document.getElementById('game-btn3').addEventListener('click', playRandomGame);
+  document.getElementById('games-info').addEventListener('click', gamesExplained);
 }
 
 function gamesExplained(event) {
   let game = document.getElementById('game-area');
   game.classList.add('explanation');
-  game.innerHTML= `
+  game.innerHTML = `
   <div class="bigger">
     <h2>Original</h2>
     <p>In this game you will play the normal game, selecting 1 card and playing against the computer to see who won, both having all the 5 cards.</p>
